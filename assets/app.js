a5ce9d0bf14590320a35db660413d8bf65365f7a
wavebrand1 <97679129+wavebrand1@users.noreply.github.com>
2026-07-16 12:38:25 +0200

    Refresh AssetMapper entrypoint for CMS styles

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,7 @@ import './stimulus_bootstrap.js';
  */
 import './styles/app.css';
 
+// Cache key for the CMS styles imported by this AssetMapper entrypoint.
+export const assetVersion = '2026-07-16-cms-1';
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
