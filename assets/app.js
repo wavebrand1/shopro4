fb739f4da58e0bb2f18fc198cf9cfb3714c46f35
wavebrand1 <97679129+wavebrand1@users.noreply.github.com>
2026-07-16 12:41:04 +0200

    Load application styles directly in page layouts

--- assets/app.js
+++ assets/app.js
@@ -5,9 +5,5 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
-
-// Cache key for the CMS styles imported by this AssetMapper entrypoint.
-export const assetVersion = '2026-07-16-cms-1';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
