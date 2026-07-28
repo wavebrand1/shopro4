61f30dbc2ef1a952b2b8afd81bc529eb6321f028
wavebrand1 <97679129+wavebrand1@users.noreply.github.com>
2026-07-28 09:38:13 +0200

    Load newsletter recipient picker directly

--- tests/Functional/AdminCmsTest.php
+++ tests/Functional/AdminCmsTest.php
@@ -2069,6 +2069,7 @@ final class AdminCmsTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertStringContainsString("import 'rich-editor'", (string) $this->client->getResponse()->getContent());
         self::assertStringNotContainsString("import 'newsletter-recipients'", (string) $this->client->getResponse()->getContent());
+        self::assertSelectorExists('script[type="module"][src*="newsletter-recipients"]');
         self::assertSelectorExists('select[data-newsletter-user-picker][multiple]');
         self::assertSelectorNotExists('#newsletter_campaign_selectedSiteUserIds input[type="checkbox"]');
     }
